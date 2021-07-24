8ec1d159778f68d884ef845e6ce5f65dc9557a38
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-07-24 20:19:22 +0700

    first project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+// const mysql = require("mysql")
+
+// const mysqlCon = mysql.createConnection({
+// 	host    : process.env.DATABASE_HOST,
+// 	user    : process.env.DATABASE_USER,
+// 	password: process.env.DATABASE_PASS,
+// 	database: process.env.DATABASE_USEDDATABASE
+// });
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+	// mysqlCon.connect(function(err) {
+	// 	if (err) throw err;
+		res.send("Connected!!!")
+	// });
+})
+
+app.listen(port, () => {
+	console.log(`App are listening on port ${port}`)
+})
