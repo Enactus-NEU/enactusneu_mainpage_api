3b685784f2c17fd5e1e8fe9a5a51dc402dd17366
Hoang Dang Khanh <hoangdangkhanh12c1@gmail.com>
2021-08-17 07:04:36 +0700

    add login-attempts

--- routes/login.route.js
+++ routes/login.route.js
@@ -14,14 +14,31 @@ router.post('/', function(req, res) {
     const password = req.body.password
     con.query(`SELECT * FROM admin WHERE email="${md5(email)}" AND password="${md5(password)}"`, function(err, result) {
         
-        if (err) return res.status(500).json({ message: err })
-        else if (result && result.length === 0) return res.sendStatus(401)
-
-        //create JWT
-        const accessToken = jwt.sign({ id: result[0].id, email: result[0].email }, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: "12h"
-        })
-        res.json({ accessToken, email })
+        if (err) {
+            res.status(500).json({ message: err })
+            return;
+        }
+        
+        if (result.length === 0) {
+            //xem co dung mail khong
+            con.query(`SELECT * FROM admin WHERE email="${md5(email)}"`, function(err, result) {
+                if (result.length !== 0 && result[0].fail_count < 10) {
+                    newCount = result[0].fail_count + 1
+                    con.query(`UPDATE admin SET fail_count=${newCount} WHERE id=${result[0].id}`)
+                }
+            })
+            return res.status(401).json({ message: "Sai email hoặc password" });
+        } 
+        
+        if (result[0].fail_count > 10) {
+            return res.status(401).json({ message: "Tài khoản đã bị khóa" })
+        } else {
+            //create JWT
+            const accessToken = jwt.sign({ id: result[0].id, email: result[0].email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "12h"
+            })
+            res.json({ accessToken, email })
+        }
     })
 })
 
